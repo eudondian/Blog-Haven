3060ce1885ca257dd5e62dc1199bdf7ff527183f
eudondian <eudondian@gmail.com>
2023-07-17 02:04:24 +0100

    Modify routes.rb files to display define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
+  # get 'posts/index'
+  get 'posts/:id', to: 'posts#show', as: :post
+  # get 'posts/show'
+  # get 'users/index'
+  get 'users/:id', to: 'users#show', as: :user
+  # get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

7623e8e4f62a7548ff54a7c7cf8a7c0ad76fe2ce
eudondian <eudondian@gmail.com>
2023-07-17 02:25:46 +0100

    Modify routes.rb file to display define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-    get 'posts/:id', to: 'posts#show', as: :post
-    get 'users/:id', to: 'users#show', as: :user
+    user_posts = get 'posts/:id', to: 'posts#show', as: :posts
+    user_post = get 'posts/:id', to: 'posts#show', as: :post
+    get 'users/:id', to: 'users#show', as: :users
+    get 'user/:id', to: 'users#show', as: :user
   end

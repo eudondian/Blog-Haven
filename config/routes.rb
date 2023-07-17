ca1cb2cf88fa6a5e3fe6ae105b946c72159518cc
eudondian <eudondian@gmail.com>
2023-07-17 11:05:32 +0100

    Fix routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-    user_posts = get 'posts/:id', to: 'posts#show', as: :posts
-    user_post = get 'post/:id', to: 'post#show', as: :post
-    get 'users/:id', to: 'users#show', as: :users
-    get 'user/:id', to: 'user#show', as: :user
-    # get 'user', to: 'users#index', as: :user
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
+end

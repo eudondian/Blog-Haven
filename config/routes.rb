c90a89f3ce285e562fd768223563f658c3fd454b
eudondian <eudondian@gmail.com>
2023-07-17 10:19:17 +0100

    Fix rubocop error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     user_posts = get 'posts/:id', to: 'posts#show', as: :posts
-    user_post = get 'posts/:id', to: 'posts#show', as: :post
+    user_post = get 'post/:id', to: 'post#show', as: :post
     get 'users/:id', to: 'users#show', as: :users
-    get 'user/:id', to: 'users#show', as: :user
+    get 'user/:id', to: 'user#show', as: :user
+    # get 'user', to: 'users#index', as: :user
   end

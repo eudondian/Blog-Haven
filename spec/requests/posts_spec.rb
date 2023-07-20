04f3e3ecc040d5f8108f0695fdbbc954d71ee395
eudondian <eudondian@gmail.com>
2023-07-20 12:15:39 +0100

    Add test to controllers for posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe 'Posts', type: :request do
+  describe 'GET #index' do
+   
+       it 'returns a successful response for post index' do
+      user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      get user_posts_path(user)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('list of all posts')
+    end
+
+    it 'returns a successful response for post index' do
+      @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+      @post = Post.create(author: @user, title: 'Hello', text: 'This is my first post')
+      get user_post_path(@user, @post)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('list of all posts')
     end
   end
 end

72cc29554c449841824c651e4247a4e38f5af47a
eudondian <eudondian@gmail.com>
2023-07-20 12:16:20 +0100

    Add test to controllers for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response for users index' do
+      get users_path
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('list of all users')
+    end
+    
+    it 'returns a successful response for users show' do
+     @user = User.create(name: 'John Doe', photo: '',  bio: 'A user from nowhere')
+      get user_path(@user)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Here are the details for the user')
     end
   end
 end

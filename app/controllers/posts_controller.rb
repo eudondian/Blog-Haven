3f6a6ac4f9740c52a6f3c01bf8b6ced7a3d86408
eudondian <eudondian@gmail.com>
2023-07-21 18:09:59 +0100

    Modify users and posts controllers file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
-  def index; end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      # Handle successful post creation
+      redirect_to @post
+    else
+      # Handle validation errors or other issues with post creation
+      render :new
+    end
+  end
 
-  def show; end
+  def index
+    @posts = Post.where(author_id: params[:user_id])
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def show
+    @post = Post.find(params[:user_id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path, alert: 'Post not found'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
 end

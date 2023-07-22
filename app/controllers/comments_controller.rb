910a8e8aa1d004f66a4d733415ffcf1b34389cc2
eudondian <eudondian@gmail.com>
2023-07-22 18:15:32 +0100

    Set path for creating and saving of data by users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+    def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.post = @post
+
+    if @comment.save
+      redirect_to users_path notice: 'Comment was successfully created.'
+    else
+      flash.now[:error] = 'Failed to create comment.'
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end

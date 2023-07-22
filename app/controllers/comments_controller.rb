dc6f231afc5f05e5e21617ce9995976b3d7daaf1
eudondian <eudondian@gmail.com>
2023-07-22 10:14:08 +0100

    Fix error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,17 @@
 class CommentsController < ApplicationController
-    def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.build(comment_params)
-        if @comment.save
-          redirect_to post_path(@post), notice: 'Comment was successfully created.'
-        else
-          redirect_to post_path(@post), alert: 'Failed to create comment.'
-        end
-      end
-    
-      private
-    
-      def comment_params
-        params.require(:comment).permit(:text)
-      end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment was successfully created.'
+    else
+      redirect_to post_path(@post), alert: 'Failed to create comment.'
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end

5f7d757d311bb4a35d90c0a935278e11b5476351
eudondian <eudondian@gmail.com>
2023-07-23 23:03:15 +0100

    Fix errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,40 +7,40 @@ class PostsController < ApplicationController
   end
 
   def create
-  @current_user = current_user
-  @post = Post.new(post_params.merge(author_id: @current_user.id))
-  respond_to do |format|
-    format.html do
-      if @post.save
-        flash[:success] = 'Post created successfully'
-        format.html { redirect_to user_post_path(@current_user, @post) }
-      else
-        flash.now[:error] = 'Error: Post could not be saved'
-        format.html { render :new }
+    @current_user = current_user
+    @post = Post.new(post_params.merge(author_id: @current_user.id))
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post created successfully'
+          format.html { redirect_to user_post_path(@current_user, @post) }
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          format.html { render :new }
+        end
       end
     end
   end
-end
 
-def index
-  @user = User.find_by(id: params[:user_id])
-  @posts = @user.posts
-end
+  def index
+    @user = User.find_by(id: params[:user_id])
+    @posts = @user.posts
+  end
 
-def likes
-  @post = Post.find(params[:id])
-  Like.create(author_id: current_user.id, post_id: @post.id)
-  redirect_to post_path(@post)
-end
+  def likes
+    @post = Post.find(params[:id])
+    Like.create(author_id: current_user.id, post_id: @post.id)
+    redirect_to post_path(@post)
+  end
 
-def show
-  @post = Post.find_by(id: params[:id])
-  redirect_to root_path, alert: 'Post not found' if @post.nil?
-end
+  def show
+    @post = Post.find_by(id: params[:id])
+    redirect_to root_path, alert: 'Post not found' if @post.nil?
+  end
 
   private
 
-def post_params
-  params.require(:post).permit(:title, :text, :user_id)
-end
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
 end
